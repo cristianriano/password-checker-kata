c2ce99c9068be1df9276b583d8d294ca3fec422b
Cristian Riano <cristian.riano@glovoapp.com>
2021-11-17 19:45:27 +0100

    Parametrize rule tests

--- src/test/java/com/cristianriano/katas/password/PasswordCheckerTest.java
+++ src/test/java/com/cristianriano/katas/password/PasswordCheckerTest.java
@@ -3,25 +3,30 @@ package com.cristianriano.katas.password;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PasswordCheckerTest {
 
-  @Test
-  void check_lengthRule() {
+  @ParameterizedTest
+  @ValueSource(strings = {"any", "", "123456", " "})
+  void check_lengthRule(String password) {
     var passwordChecker = new PasswordChecker(new LengthRule(7));
-    assertFalse(passwordChecker.check("any"));
+    assertFalse(passwordChecker.check(password));
   }
 
-  @Test
-  void check_letterRule() {
+  @ParameterizedTest
+  @ValueSource(strings = {"", " ", "123", "!@#"})
+  void check_letterRule(String password) {
     var passwordChecker = new PasswordChecker(new LetterRule());
-    assertFalse(passwordChecker.check("123"));
+    assertFalse(passwordChecker.check(password));
   }
 
-  @Test
-  void check_digitRule() {
+  @ParameterizedTest
+  @ValueSource(strings = {"", " ", "abc", "!@#"})
+  void check_digitRule(String password) {
     var passwordChecker = new PasswordChecker(new DigitRule());
-    assertFalse(passwordChecker.check("abc"));
+    assertFalse(passwordChecker.check(password));
   }
 
   @Test
